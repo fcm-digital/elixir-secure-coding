dd8554b8456b31fc447a08644a5f07cf8a2e67f7
Rock <miguel.garcia@fcm.digital>
2025-08-12 16:05:08 +0200

    Track results locally

--- modules/grading_client/lib/grading_client/answers.ex
+++ modules/grading_client/lib/grading_client/answers.ex
@@ -46,27 +46,70 @@ defmodule GradingClient.Answers do
     GenServer.call(__MODULE__, :get_modules)
   end
 
+  def get_results() do
+    GenServer.call(__MODULE__, :get_results)
+  end
+
   @impl true
   def handle_call(:get_modules, _from, state) do
     {:reply, state.modules, state}
   end
 
   @impl true
+  def handle_call(:get_results, _from, state) do
+    data = :ets.lookup(:answer_store, :results)
+
+    results = data[:results] || %{}
+
+    all_data = :ets.match(:answer_store, {{:"$1", :"$2"}, :"$3"})
+
+    results =
+      all_data
+      |> Enum.sort_by(fn [module, task, _] -> {module, task} end)
+      |> Enum.map(fn [module, task, _] ->
+        base = %{"module" => module, "task" => task}
+
+        case Map.get(results, {module, task}) do
+          nil -> Map.put(base, "result", "❓ Not answered")
+          :incorrect -> Map.put(base, "result", "❌ Incorrect")
+          :correct -> Map.put(base, "result", "✅ Correct")
+        end
+      end)
+
+    {:reply, results, state}
+  end
+
+  @impl true
   def handle_call({:check, module_id, question_id, answer}, _from, state) do
+    key = {module_id, question_id}
+
     result =
       case :ets.lookup(@table, {module_id, question_id}) do
         [] ->
           {:incorrect, "Question not found"}
 
         [{_id, %Answer{answer: correct_answer, help_text: help_text}}] ->
-
           if answer == correct_answer do
+            update_results(key, :correct)
             :correct
           else
+            update_results(key, :incorrect)
             {:incorrect, help_text}
           end
       end
 
     {:reply, result, state}
+  end
+
+  defp update_results(key, result) do
+    results =
+      case :ets.lookup(@table, :results) do
+        [] -> %{}
+        [results: results] -> results
+      end
+
+    results = Map.put(results, key, result)
+
+    :ets.insert(@table, {:results, results})
   end
 end
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+echo "Start grading client:"
+echo "cd modules/grading_client"
+echo "mix deps.get"
+echo "iex --name ts@127.0.0.1 --cookie mycookie -S mix"
+
+LIVEBOOK_DEFAULT_RUNTIME=attached:ts@127.0.0.1:mycookie LIVEBOOK_PASSWORD=123412341234 livebook server
